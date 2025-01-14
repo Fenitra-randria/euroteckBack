c88e9d71e1753689daf36b0db596111649032f8d
Fenitra Randriamihajanirina <fenitra.randriamihajanirina@pulse.mg>
2025-01-14 16:13:33 +0300

    dynamisation nouveauté

--- src/main/java/com/eurotec/backend/repository/ProduitRepository.java
+++ src/main/java/com/eurotec/backend/repository/ProduitRepository.java
@@ -16,81 +16,67 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @SecurityRequirement(name = "bearerAuth")
 @Repository
-public interface ProduitRepository extends JpaRepository<Produit, Long>
-{
-	
-	List<Produit> findByBoutiqueId(Long id);	
-	
-	@Query(value = """
-					SELECT p 
-					 FROM Produit p 
-					 where (p.boutique.id = :id) 
-					 and ( p.nom like %:search% or p.codeBarre like %:search% ) 
-				""")
-	List<Produit> rechercherCommande(@Param("search") String search , @Param("id") Long id , Pageable page);
-	
+public interface ProduitRepository extends JpaRepository<Produit, Long> {
+
+	List<Produit> findByBoutiqueId(Long id);
 
 	@Query(value = """
-				SELECT p 
-				FROM Produit p 
-				where (p.boutique.id = :id) 
-				and ( p.nom like %:search% or p.codeBarre like %:search% ) 
-				
+				SELECT p
+				 FROM Produit p
+				 where (p.boutique.id = :id)
+				 and ( p.nom like %:search% or p.codeBarre like %:search% )
 			""")
-	List<Produit> rechercherSimple(@Param("search") String search , @Param("id") Long id , Pageable page);
-	
-	
+	List<Produit> rechercherCommande(@Param("search") String search, @Param("id") Long id, Pageable page);
+
 	@Query(value = """
-			SELECT p
-			 FROM Produit p join p.boutique b
-			 where ( p.nom like %:search% or p.codeBarre like %:search%  ) 
-			 and b.id = 17
-		""")
-	List<Produit> rechercherTout(@Param("search") String search  , Pageable page);
-		
+				SELECT p
+				FROM Produit p
+				where (p.boutique.id = :id)
+				and ( p.nom like %:search% or p.codeBarre like %:search% )
+
+			""")
+	List<Produit> rechercherSimple(@Param("search") String search, @Param("id") Long id, Pageable page);
+
 	@Query(value = """
-			SELECT p
-			 FROM Produit p join p.boutique b
-			 where ( p.famille like :search ) 
-			 and b.id = 17
-		""")
-	List<Produit> rechercherParCategorie(@Param("search") String search  , Pageable page);
-	
+				SELECT p
+				 FROM Produit p join p.boutique b
+				 where ( p.nom like %:search% or p.codeBarre like %:search%  )
+			""")
+	List<Produit> rechercherTout(@Param("search") String search, Pageable page);
+
 	@Query(value = """
-			SELECT p
-			 FROM Produit p join p.boutique b
-			 where ( p.nouveaute = true  ) 
-			 and b.id = 17
-		""")
-	List<Produit> rechercherNouveaute( Pageable page);
-	
-	
+				SELECT p
+				 FROM Produit p join p.boutique b
+				 where ( p.famille like :search )
+			""")
+	List<Produit> rechercherParCategorie(@Param("search") String search, Pageable page);
+
 	@Query(value = """
-			SELECT p
-			 FROM Produit p join p.boutique b
-			 where ( p.promotion = true  ) 
-			 and b.id = 17
-		""")
-	List<Produit> rechercherPromotion( Pageable page);
-	
-	
+				SELECT p
+				 FROM Produit p join p.boutique b
+				 where ( p.nouveaute = true  )
+			""")
+	List<Produit> rechercherNouveaute(Pageable page);
+
 	@Query(value = """
-			SELECT p
-			 FROM Produit p join p.boutique b
-			 where ( p.retourEnStock = true  ) 
-			 and b.id = 17
-		""")
-	List<Produit> rechercherRetourEnStock( Pageable page);
-	
-	
-	
+				SELECT p
+				 FROM Produit p join p.boutique b
+				 where ( p.promotion = true  )
+			""")
+	List<Produit> rechercherPromotion(Pageable page);
+
 	@Query(value = """
-			SELECT DISTINCT p.famille
-			 FROM Produit p join p.boutique b
-			where (p.famille is not null) and (p.famille <> '') 
-			and b.id = 17
-		""")
+				SELECT p
+				 FROM Produit p join p.boutique b
+				 where ( p.retourEnStock = true  )
+			""")
+	List<Produit> rechercherRetourEnStock(Pageable page);
+
+	@Query(value = """
+				SELECT DISTINCT p.famille
+				 FROM Produit p join p.boutique b
+				where (p.famille is not null) and (p.famille <> '')
+			""")
 	List<String> rechercherCategorie();
-		
 
 }
